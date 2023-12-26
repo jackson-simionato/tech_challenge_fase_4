ad4fd98e7139f5f906264b7a62ba5a65eaf5d605
jackson-simionato <simionato.jackson@gmail.com>
2023-12-26 08:39:05 -0300

    Implement google big query connection

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ import pandas as pd
 api_key = local_env.eia_api_key
 api_url = local_env.eia_api_url
 
+project_id = 'brave-tea-400210'
+dataset_id = 'eia_oil_data'
+
 def check_dir(path):
     """
     This function checks if a directory exists at the specified path, and creates it if it doesn't.
@@ -17,7 +20,7 @@ def check_dir(path):
     if not os.path.exists(path):
         return os.makedirs(path)
 
-def get_fob_data(api_url, api_key):
+def get_fob_data(api_url, api_key, project_id, dataset_id):
     # URL da requisição
     url = f'{api_url}/petroleum/pri/spt/data/?api_key={api_key}&frequency=daily&data[0]=value&facets[product][]=EPCBRENT&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000'
     response = requests.get(url)
@@ -49,12 +52,21 @@ def get_fob_data(api_url, api_key):
     df.reset_index(drop=True, inplace=True)
 
     # Exportação
+    fname = 'eia_fob_price_EPCBRENT.csv'
+
     check_dir('data/eia_fob_price')
-    df.to_csv('data/eia_fob_price/eia_fob_price_EPCBRENT.csv', index=False)
+
+    df.to_csv(f'data/eia_fob_price/{fname}', index=False)
+
+    # Upload para Google Big Query
+    table_id = fname.split('.')[0]
+
+    # Use the pandas_gbq.to_gbq() function to insert the DataFrame into BigQuery
+    pd.io.gbq.to_gbq(df, f'{project_id}.{dataset_id}.{table_id}', project_id=project_id, if_exists='replace')
 
     return df
 
-def get_petroleum_production_data(api_url, api_key):
+def get_petroleum_production_data(api_url, api_key, project_id, dataset_id):
     # URL da requisição
     url = f'{api_url}/international/data/?api_key={api_key}&frequency=monthly&data[0]=value&facets[activityId][]=1&facets[productId][]=53&facets[countryRegionId][]=WORL&facets[unit][]=TBPD&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000'
     response = requests.get(url)
@@ -86,12 +98,21 @@ def get_petroleum_production_data(api_url, api_key):
     df.reset_index(drop=True, inplace=True)
 
     # Exportação
+    fname = 'eia_monthly_oil_production_world.csv'
+
     check_dir('./data/eia_oil_production')
-    df.to_csv('./data/eia_oil_production/eia_monthly_oil_production_world.csv', index=False)
+    
+    df.to_csv(f'./data/eia_oil_production/{fname}', index=False)
+
+    # Upload para Google Big Query
+    table_id = fname.split('.')[0]
+
+    # Use the pandas_gbq.to_gbq() function to insert the DataFrame into BigQuery
+    pd.io.gbq.to_gbq(df, f'{project_id}.{dataset_id}.{table_id}', project_id=project_id, if_exists='replace')
 
     return df
 
-def get_energy_consumption_data(api_url, api_key):
+def get_energy_consumption_data(api_url, api_key, project_id, dataset_id):
     # URL da requisição
     url = f'{api_url}/international/data/?api_key={api_key}&frequency=annual&data[0]=value&facets[activityId][]=2&facets[productId][]=44&facets[countryRegionId][]=WORL&facets[unit][]=QBTU&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000'
     response = requests.get(url)
@@ -123,12 +144,21 @@ def get_energy_consumption_data(api_url, api_key):
     df.reset_index(drop=True, inplace=True)
 
     # Exportação
+    fname = 'eia_annual_energy_consumption_world.csv'
+
     check_dir('./data/eia_energy_consumption')
-    df.to_csv('./data/eia_energy_consumption/eia_annual_energy_consumption_world.csv', index=False)
+
+    df.to_csv(f'./data/eia_energy_consumption/{fname}', index=False)
+
+    # Upload para Google Big Query
+    table_id = fname.split('.')[0]
+
+    # Use the pandas_gbq.to_gbq() function to insert the DataFrame into BigQuery
+    pd.io.gbq.to_gbq(df, f'{project_id}.{dataset_id}.{table_id}', project_id=project_id, if_exists='replace')
 
     return df
 
-def get_oil_consumption_data(api_url, api_key):
+def get_oil_consumption_data(api_url, api_key, project_id, dataset_id):
     # URL da requisição
     url = f'{api_url}/international/data/?api_key={api_key}&frequency=annual&data[0]=value&facets[activityId][]=2&facets[productId][]=5&facets[countryRegionId][]=WORL&facets[unit][]=TBPD&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000'
     response = requests.get(url)
@@ -157,15 +187,37 @@ def get_oil_consumption_data(api_url, api_key):
 
         c += 1
 
+    df = df.loc[df['value'] != '--']
+    df['value'] = df['value'].astype(float)
+    df.drop(columns=['dataFlagId','dataFlagDescription'], inplace=True)
+
     df.reset_index(drop=True, inplace=True)
 
     # Exportação
+    fname = 'eia_annual_oil_consumption_world.csv'
+
     check_dir('./data/eia_oil_consumption')
-    df.to_csv('./data/eia_oil_consumption/eia_annual_oil_consumption_world.csv', index=False)
+
+    df.to_csv(f'./data/eia_oil_consumption/{fname}', index=False)
+
+    # Upload para Google Big Query
+    table_id = fname.split('.')[0]
+
+    print(df.dtypes)
+
+    # Use the pandas_gbq.to_gbq() function to insert the DataFrame into BigQuery
+    pd.io.gbq.to_gbq(df, f'{project_id}.{dataset_id}.{table_id}', project_id=project_id, if_exists='replace')
 
     return df
 
-get_fob_data(api_url, api_key)
-get_petroleum_production_data(api_url, api_key)
-get_energy_consumption_data(api_url, api_key)
-get_oil_consumption_data(api_url, api_key)
+def get_eia_data(api_url, api_key, project_id, dataset_id):
+
+    get_fob_data(api_url, api_key, project_id, dataset_id)
+    get_petroleum_production_data(api_url, api_key, project_id, dataset_id)
+    get_energy_consumption_data(api_url, api_key, project_id, dataset_id)
+    get_oil_consumption_data(api_url, api_key, project_id, dataset_id)
+
+    return print('Aquisição de dados finalizada com sucesso!')
+
+#get_eia_data(api_url, api_key, project_id, dataset_id)
+get_oil_consumption_data(api_url, api_key, project_id, dataset_id)
